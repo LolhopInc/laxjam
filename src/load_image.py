02dad23cfc0961bfc34469901f4e2cdbb625dbc0
Patrick Higgins <quickpanic@me.com>
2015-07-13 00:20:56 -0700

    Added load_image.py
    Function to simplify image-loading tasks

---
+++ src/load_image.py
@@ -0,0 +1,17 @@
+#load_image.py
+
+#Patrick Higgins
+#Shorthand to load image from assets directory
+
+import os, sys, pygame
+
+def load_image(name):
+    fullname = os.path.join('assets', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', name
+        raise SystemExit, message
+    image = image.convert()
+
+    return image
